81cce5a5795f7bae3896d9be79912f4c2549b986
alamincoders <developeralamin03@gmail.com>
2022-10-09 11:27:36 +0600

    settings page complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
+import { Box, Typography } from "@mui/material";
+import Container from "@mui/material/Container";
 import { Route, Routes } from "react-router-dom";
 import FinalScreen from "./pages/FinalScreen";
 import Questions from "./pages/Questions";
 import Settings from "./pages/Settings";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Settings />} />
-      <Route path="/settings" element={<Settings />} />
-      <Route path="/questions" element={<Questions />} />
-      <Route path="/final" element={<FinalScreen />} />
-    </Routes>
+    <Container maxWidth="sm">
+      <Box textAlign="center" mt={5}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Typography variant="h2" fontWeight="bold">
+                  Quiz App
+                </Typography>
+                <Settings />
+              </>
+            }
+          />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/final" element={<FinalScreen />} />
+        </Routes>
+      </Box>
+    </Container>
   );
 }
 
 export default App;
-
--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,7 +1,26 @@
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import SelectField from "../components/SelectField";
+import TextFieldComp from "../components/TextFieldComp";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <SelectField label="Category" />
+      <SelectField label="Difficulty" />
+      <SelectField label="Type" />
+      <TextFieldComp />
+      <Box mt={3} width="100%">
+        <Button fullWidth variant="contained" type="submit">
+          Get Started
+        </Button>
+      </Box>
+    </form>
+  );
 };
 
 export default Settings;
