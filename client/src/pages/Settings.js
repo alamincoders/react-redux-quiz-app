4fe00db83cdb32181367384daee18d3b5c68bfbd
alamincoders <developeralamin03@gmail.com>
2022-10-09 11:50:54 +0600

    custom hook create and fetch api

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,18 +1,47 @@
-import { Button } from "@mui/material";
+import { Button, CircularProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import SelectField from "../components/SelectField";
 import TextFieldComp from "../components/TextFieldComp";
+import useAxios from "../hooks/useAxios";
 
 const Settings = () => {
+  const { response, error, loading } = useAxios({ url: "api_category.php" });
+
+  if (loading) {
+    return (
+      <Box mt={20}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Typography variant="h6" mt={20} color="red">
+        {error}
+      </Typography>
+    );
+  }
+
+  const difficultyOptions = [
+    { id: "easy", name: "Easy" },
+    { id: "medium", name: "Medium" },
+    { id: "hard", name: "Hard" },
+  ];
+  const typeOptions = [
+    { id: "multiple", name: "Multiple Choice" },
+    { id: "boolean", name: "True/False" },
+  ];
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
   return (
     <form onSubmit={handleSubmit}>
-      <SelectField label="Category" />
-      <SelectField label="Difficulty" />
-      <SelectField label="Type" />
+      <SelectField options={response.trivia_categories} label="Category" />
+      <SelectField options={difficultyOptions} label="Difficulty" />
+      <SelectField options={typeOptions} label="Type" />
       <TextFieldComp />
       <Box mt={3} width="100%">
         <Button fullWidth variant="contained" type="submit">
